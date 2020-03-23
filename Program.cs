dd13186782f892ce6f4901e05e7fd9c00662361a
Oleg Diev <defar6@yandex.ru>
2020-03-23 17:34:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AreaForm;
+using AreaForm.Figures;
+
+namespace TestForMindbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circ = new Circle(6);
+            Triangle triangle = new Triangle(3,4,5);
+
+            Console.WriteLine("Круг площадь " + circ.GetAreaFigure + " радиус "+circ.Radius);
+            Console.WriteLine("Треугольник площадь " + triangle.GetAreaFigure + " правильный " + triangle.RightTriangle);
+            Console.ReadLine();
+        }
+    }
+}
